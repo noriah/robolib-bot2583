6daa53ade4d38e9b5818dd8edf82a8d43f36878d
noriah <vix@noriah.dev>
2015-01-20 19:47:19 -0600

    This should fix some issues. Requires downloading robolibj and running the 'full' target on the ant script.

--- src/org/team2583/rcyclrsh/WestwoodBot.java
+++ src/org/team2583/rcyclrsh/WestwoodBot.java
@@ -15,7 +15,15 @@
 
 package org.team2583.rcyclrsh;
 
+import io.github.robolib.control.Joystick.JSID;
+import io.github.robolib.control.XBoxController;
 import io.github.robolib.framework.RoboLibBot;
+import io.github.robolib.iface.PWM.PWMChannel;
+import io.github.robolib.output.Victor;
+import io.github.robolib.robot.TeleopMode;
+import io.github.robolib.util.PDP.PowerChannel;
+
+import edu.wpi.first.wpilibj.tables.ITable;
 
 /**
  * 
@@ -23,12 +31,67 @@ import io.github.robolib.framework.RoboLibBot;
  */
 public class WestwoodBot extends RoboLibBot {
     
+    private Victor motor1, motor2;
+    private XBoxController stick0;
+    ITable table;
+
     public WestwoodBot(){
         super("Stacker", "1.0.0");
+        enableDebug(true);
     }
-    
+
     public void robotInit(){
-        
+       motor1 = new Victor(PWMChannel.Channel0, "Left Motor", PowerChannel.Channel0);
+       motor2 = new Victor(PWMChannel.Channel1, "Right Motor", PowerChannel.Channel3);
+       motor1.enableSafety(false);
+       motor2.enableSafety(false);
+       motor2.setInverted(true);
+
+        stick0 = new XBoxController(JSID.JS0);
+        table = getRobotTable().getSubTable("JS");
+        table.putNumber("JS Scaler", 0.75);
+        new TeleopMode(){
+
+           double a, b, c, d;
+            public void run(){
+                a = b = stick0.getLeftY();
+                if(a < 0){
+                    b = -a;
+                }
+                a = a * b;
+
+                c = d = stick0.getRightX();
+                if(c < 0){
+                    d = -c;
+                }
+
+                c = c * d;
+
+                if(a > 0){
+                    if(c > 0){
+                        b = a - c;
+                        d = Math.max(a, c);
+                    }else{
+                        b = Math.max(a, -c);
+                        d = a + c;
+                    }
+                }else{
+                    if(c > 0){
+                        b = -Math.max(-a, c);
+                        d = a + c;
+                    }else{
+                        b = a - c;
+                        d = -Math.max(-a, -c);
+                    }
+                }
+
+               b = b * table.getNumber("JS Scaler");
+               d = d * table.getNumber("JS Scaler");
+
+               motor1.setSpeed(d);
+               motor2.setSpeed(b);
+            }
+        };
     }
 
 }
---
+++ src/org/team2583/rcyclrsh/control/ArcadeController.java
@@ -0,0 +1,35 @@
+package org.team2583.rcyclrsh.control;
+
+import io.github.robolib.control.Joystick;
+
+
+/**
+ *
+ * @author noriah
+ */
+public class ArcadeController extends Joystick{
+        
+    public static final int BUTTON_ROLLERS_FORWARD = 13;
+    public static final int BUTTON_ROLLERS_BACK = 14;
+    public static final int BUTTON_CATCHER_CLOSE = 5;
+    public static final int BUTTON_CATCHER_OPEN = 4;
+    public static final int BUTTON_FLIPPER_UP = 5;
+    public static final int BUTTON_FLIPPER_DOWN = 6;
+    public static final int BUTTON_LAUNCHER_EXTEND = 10;
+    public static final int BUTTON_LAUNCHER_RETRACT = 9;
+    public static final int BUTTON_RELEASE_LATCH = 11;
+    public static final int BUTTON_RELEASE_UNLATCH = 12;
+    
+    public static final int BUTTON_FIRE = 1;
+    public static final int BUTTON_EJECT_BALL = 7;
+    public static final int BUTTON_PREP_PICKUP = 6;
+    public static final int BUTTON_REPREP = 8;
+    public static final int BUTTON_CATCHER_TOGGLE = 3;
+    public static final int BUTTON_FLIPPER_TOGGLE = 2;
+    
+    public ArcadeController(final JSID port){
+        super(port);
+        addAxisButton(1, 0.5, -1.0);
+        addAxisButton(2, 0.5, -1.0);
+    }
+}
